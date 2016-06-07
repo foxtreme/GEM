b92523c7978f1aa40b4544aa452cc72ebfb5fd48
chris <chris@localhost>
2016-06-06 21:13:07 -0500

    pruebas de bondad comentadas

--- src/generador/Gem.java
+++ src/generador/Gem.java
@@ -127,7 +127,7 @@ public class Gem {
             for (int i = 0; i < n; i++) {
                 double ri = generarAleatorio();
                 System.out.print(ri + " , ");
-                if (ri <= p) {
+                if (ri <= p) {//el numero es un exito
                     numBinomial++;
                     numerosBinomial.add(numBinomial);
                 }
@@ -144,18 +144,24 @@ public class Gem {
         return numerosBinomial;
     }
 
+    /**
+     * Aplica la prueba de chi cuadrado a una lista de numeros pseudoaleatorios
+     * @param lista con los numeros pseudoaleatorios
+     * @return true si pasa la prueba y false si la lista de numeros es rechazada
+     */
     public boolean pruebachi2(List<Double> lista) {
+        System.out.println("----------Prueba Chi cuadrado----------");
         double chicrit = 0;
-        double cant = lista.size();
+        double cant = lista.size();//cantidad de datos
         if (cant == 1000) {
-            chicrit = 41.4217;
+            chicrit = 41.4217;//de acuerdo a la tabla y al numero de clases que determinan los grados de libertad
         }
         if (cant == 10000) {
-            chicrit = 118.4980;
+            chicrit = 118.4980;//de acuerdo a la tabla y al numero de clases que determinan los grados de libertad
         }
-        double clases = ceil(sqrt(cant));
-        double gl = clases - 1;
-        double FE = cant / clases;
+        double clases = ceil(sqrt(cant));//numero de clases
+        double gl = clases - 1;//numero de grados de libertad
+        double FE = cant / clases; //frecuencia esperada en cada intervalo
         System.out.println("clases: " + clases);
         System.out.println("gl: " + gl);
         System.out.println("Rango \t\t\t\t FO \t\t FE \t\t (FE-FO)^2/FE");
@@ -165,33 +171,40 @@ public class Gem {
             int FO = countOnRange(i, (i + (1 / clases)), lista);
             FOsum += FO;
             double chicalci = pow((FE - FO), 2) / FE;
-            chicalc += pow((FE - FO), 2) / FE;
+            chicalc += pow((FE - FO), 2) / FE;//chi calculado
             System.out.println("[" + i + " - " + (i + (1 / clases)) + ")" + "\t\t" + FO + "\t\t" + FE + "\t\t" + chicalci);
         }
         System.out.println("Chi critico: " + chicrit);
         System.out.println("Chi calculado: " + chicalc);
         System.out.println("FO Total: " + FOsum);
+        System.out.println("Pasa la prueba?: ");
         return chicalc <= chicrit;
     }
 
+    /**
+     * Aplica la prueba de poker con 3 decimales a una lista de numeros pseudoaleatorios
+     * @param lista con los numeros pseudoaleatorios
+     * @return true si pasa la prueba, false si la lista de numeros es rechazada
+     */
     public boolean poker3(List<Double> lista) {
-        List<Double> listan = formatList(lista, "#.###");
+        System.out.println("----------Prueba de Poker 3 decimales----------");
+        List<Double> listan = formatList(lista, "#.###");//define el formato para 3 decimales
         System.out.println("Clase \t\t\t\t FO \t\t FE \t\t x2");
         int Iguales3 = 0;
         int Iguales2 = 0;
         int Iguales0 = 0;
-        double chicrit = 4.6052;
+        double chicrit = 4.6052;//de acuerdo a la tabla
 
         for (Double number : listan) {
             double num = ceil(number * 1000);
             double num1 = (int) num / 100;
             double num2 = ((int) num / 10) % 10;
             double num3 = num % 10;
-            if (num1 == num2 && num1 == num3) {
+            if (num1 == num2 && num1 == num3) {//los 3 numeros son iguales
                 Iguales3++;
-            } else if (num1 != num2 && num1 != num3 && num2 != num3) {
+            } else if (num1 != num2 && num1 != num3 && num2 != num3) {//los 3 numeros son diferentes
                 Iguales0++;
-            } else {
+            } else {//hay dos numeros iguales
                 Iguales2++;
             }
         }
@@ -199,40 +212,46 @@ public class Gem {
         double chi21 = (pow((FE * 0.01) - Iguales3, 2)) / (FE * 0.01);
         double chi22 = (pow((FE * 0.27) - Iguales2, 2)) / (FE * 0.27);
         double chi23 = (pow((FE * 0.72) - Iguales0, 2)) / (FE * 0.72);
-        double chicalc = chi21 + chi22 + chi23;
+        double chicalc = chi21 + chi22 + chi23;//se calcula el chi cuadrado
         System.out.println("3 Iguales                \t " + Iguales3 + " \t\t " + FE * 0.01 + " \t\t " + chi21);
         System.out.println("2 Iguales, 1 Diferente   \t " + Iguales2 + " \t\t " + FE * 0.27 + " \t\t " + chi22);
         System.out.println("3 Diferentes             \t " + Iguales0 + " \t\t " + FE * 0.72 + " \t\t " + chi23);
         System.out.println("\t\t\t\t\t\t chicalc:        " + chicalc);
-
+        System.out.println("Pasa la prueba?: ");
         return chicalc <= chicrit;
     }
 
+    /**
+     * Aplica la prueba de poker con 2 decimales a una lista de numeros pseudoaleatorios
+     * @param lista con los numeros pseudoaleatorios
+     * @return true si pasa la prueba, false si la lista de numeros es rechazada
+     */
     public boolean poker2(List<Double> lista) {
+        System.out.println("----------Prueba de Poker 2 decimales----------");
         List<Double> listan = formatList(lista, "#.##");
         System.out.println("Clase \t\t\t\t FO \t\t FE \t\t x2");
         int Iguales2 = 0;
         int Iguales0 = 0;
-        double chicrit =  2.7055;
+        double chicrit =  2.7055;//de acuerdo a la tabla
 
         for (Double number : listan) {
             double num = ceil(number * 100);
             double num1 = ((int) num / 10);
             double num2 = num % 10;
-            if (num1 == num2) {
+            if (num1 == num2) {//los dos decimales son iguales
                 Iguales2++;
-            } else {
+            } else {//los dos decimales son diferentes
                 Iguales0++;
             }
         }
         double FE = listan.size();
         double chi21 = (pow((FE * 0.1) - Iguales2, 2)) / (FE * 0.1);
         double chi22 = (pow((FE * 0.9) - Iguales0, 2)) / (FE * 0.9);
-        double chicalc = chi21 + chi22;
+        double chicalc = chi21 + chi22;//se calcula el chi cuadrado
         System.out.println("2 Iguales      \t\t\t " + Iguales2 + " \t\t " + FE * 0.1 + " \t\t " + chi21);
         System.out.println("2 Diferentes   \t\t\t " + Iguales0 + " \t\t " + FE * 0.9 + " \t\t " + chi22);
         System.out.println("\t\t\t\t\t\t chicalc:        " + chicalc);
-        
+        System.out.println("Pasa la prueba?: ");
         return chicalc <= chicrit;
     }
 
@@ -246,6 +265,11 @@ public class Gem {
         return cant;
     }
 
+    /**
+     * Genera los archivos con los numeros pseudoaleatorios
+     * @return los numeros generados en la lista
+     * @throws IOException 
+     */
     public List<Double> generarLista() throws IOException {
         List<Double> lines = new LinkedList<>();
         List<String> linesS = new LinkedList<>();
@@ -264,8 +288,11 @@ public class Gem {
         return lines;
     }
 
-    /*
-    Formatear numeros a un determinado numero de decimales   
+    /**
+     * Formatea los numeros a un determinado numero de decimales
+     * @param lista lista de numeros pseudoaleatorios
+     * @param decimales cantidad de decimales aceptados
+     * @return Lista de numeros pseudoaleatorios formateados
      */
     private List<Double> formatList(List<Double> lista, String decimales) {
         List<Double> newList = new LinkedList<>();
